ea21f3c4ef70361fd40fc1c089376c0e6bdcae00
Gabriel Silva Lima <gabriel.osilvalima@gmail.com>
2021-05-24 20:18:17 -0300

    Piece e Board

--- src/main/java/JogoDeXadrez/Application.java
+++ src/main/java/JogoDeXadrez/Application.java
@@ -1,7 +1,12 @@
 package JogoDeXadrez;
 
+import JogoDeXadrez.BoardGame.Board;
+
 public class Application {
     public static void main(String[] args) {
+
+        Board board = new Board(8,8);
+
 
     }
 }
